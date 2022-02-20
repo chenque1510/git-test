d634fae02e099a235d3652944d19c8495f8eb293
sofa <chenshaohuapku@163.com>
2022-02-20 11:14:53 +0800

    first commit

---
+++ src/pku/chen/GitTest.java
@@ -0,0 +1,11 @@
+package pku.chen;
+
+/**
+ * @author cshstart
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
